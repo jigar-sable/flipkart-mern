8e0ec26fec2a89e3130775d77e7364683112d67d
Jigar Sable <64949957+jigar-sable@users.noreply.github.com>
2023-03-04 20:02:55 +0530

    minor fix

--- frontend/src/components/Admin/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard.jsx
@@ -20,7 +20,7 @@ const Dashboard = ({ activeTab, children }) => {
                 {!onMobile && <Sidebar activeTab={activeTab} />}
                 {toggleSidebar && <Sidebar activeTab={activeTab} setToggleSidebar={setToggleSidebar}/>}
 
-                <div className="w-full sm:w-4/5 sm:ml-64 min-h-screen">
+                <div className="w-full sm:w-4/5 sm:ml-72 min-h-screen">
                     <div className="flex flex-col gap-6 sm:m-8 p-2 pb-6 overflow-hidden">
                         <button onClick={() => setToggleSidebar(true)} className="sm:hidden bg-gray-700 w-10 h-10 rounded-full shadow text-white flex items-center justify-center"><MenuIcon /></button>
                         {children}
